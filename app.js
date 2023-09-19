74738545219752b7480effab4deb5f877cfe272b
Márcio Silva <139601887+marciocapendeli@users.noreply.github.com>
2023-09-19 17:08:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação esta se cpmectamdp ap banco de dados")
+console.log("aplicação esta se conectando ap banco de dados")
